297b349a1d2880031bf47baa2c96cc6ab8e30f40
Astrocat2374 <sugiyama.evan@gmail.com>
2018-03-05 11:22:32 -1000

    Test

---
+++ app.js
@@ -0,0 +1,15 @@
+var question10 = [
+	"first",
+	"second",
+	"third",
+	"fourth"
+];
+
+function removeLast () {
+	//Umm... I can't remember. Is it like... hmm...
+	var i = array.length(question10) - 2
+	var question10 = 1
+	return question10[3]
+}
+
+console.log(removeLast())
